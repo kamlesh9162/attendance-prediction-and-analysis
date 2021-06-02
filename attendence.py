f6efc6a4f4f5a6b461b35d57941b16a2b37adbc9
Kamlesh Mundel <53644784+kamlesh9162@users.noreply.github.com>
2021-06-02 05:33:10 -0400

    Update attendence.py

--- attendence.py
+++ attendence.py
@@ -1,17 +1,25 @@
+# Required modules
 import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Importing sheet and converting in DataFrame
 exl = pd.read_excel('<path of excel sheet>',sheet_name='<name of sheet>')
 df = pd.DataFrame(exl)
 
+# Function for prediction
 def pred(n):
     total = df.at[n-1,'Jan total present']+df.at[n-1,'Jan total absent']+df.at[n-1,'Feb total present']+df.at[n-1,'Feb total absent']+df.at[n-1,'March total present']+df.at[n-1,'March total absent']+df.at[n-1,'April total present']+df.at[n-1,'April total absent']+df.at[n-1,'May total present']+df.at[n-1,'May total absent']
+    # Total presents
     tp = df.at[n-1,'Jan total present']+df.at[n-1,'Feb total present']+df.at[n-1,'March total present']+df.at[n-1,'April total present']+df.at[n-1,'May total present']
+    # Input upcoming classes
     ucc = int(input('Enter the no. of Up coming classes : '))
+    # 75% of total classes
     sf = int((total+ucc)*0.75)
+    # Minimum attendence required
     ma = sf - tp
+    
     if ma<=0:
         print('You can skip them all')
     else:
@@ -21,16 +29,18 @@ def pred(n):
         else:
             print('You should attend',ma,'more classes')
 
+# Function to get graphical analysis
 def attend():
     n = int(input('Enter the roll no. of sutdent : '))
     att = np.array([round(df.at[n-1,'Jan \n%'],2),round(df.at[n-1,'Feb %'],2),round(df.at[n-1,'March %'],2),round(df.at[n-1,'April %'],2),round(df.at[n-1,'May%'],2),round(df.at[n-1,'Total'],2)])
     mnt = np.array(['Jan','Feb','March','April','May','Total'])
     clr = np.array(['blue','green','yellow','black','cyan','red'])
-
+    
+    # Ploting the Data
     plt.bar(mnt,att,color=clr)
     plt.title(df.at[n-1,'Name of Student'],fontsize=15)
 
-
+    # Getting precentage on the bars
     for index,data in enumerate(att):
         plt.text(x=index , y =data+1 , s=f"{data}" , fontdict=dict(fontsize=10))
     plt.tight_layout()
@@ -38,4 +48,5 @@ def attend():
     pred(n)
     attend()
 
+# Calling the function to run program
 attend()
